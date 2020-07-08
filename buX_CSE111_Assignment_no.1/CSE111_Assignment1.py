98fbd208dbd77386e9bea6baf102da3692a933a9
munad69 <51168503+munad69@users.noreply.github.com>
2020-07-09 01:20:26 +0600

    Update CSE111_Assignment1.py

--- buX_CSE111_Assignment_no.1/CSE111_Assignment1.py
+++ buX_CSE111_Assignment_no.1/CSE111_Assignment1.py
@@ -120,7 +120,31 @@ print(len(unique))
 print(" ")
 
 #2
-print("PARI NA BAAAAL")
+print("Answer of Hard Number 2")
+inpt = int(input("Enter your number: "))
+n = 0
+for i in range(1, inpt + 1):  
+    for j in range (1, (inpt - i) + 1): 
+        print(end = " ") 
+          
+    while n != (2 * i - 1): 
+        print("*", end = "") 
+        n = n + 1
+    n = 0
+    print()  
+  
+k = 1
+n = 1
+for i in range(1, inpt):  
+    for j in range (1, k + 1): 
+        print(end = " ") 
+    k = k + 1
+           
+    while n <= (2 * (inpt - i) - 1): 
+        print("*", end = "") 
+        n = n + 1
+    n = 1
+    print() 
 print(" ")
 
 #3
